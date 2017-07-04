1f9c0857bef7537f25d05c51ef4c4c23146a6a22
Duong Le Ha <duongleha212001@gmail.com>
2017-07-04 20:08:13 +0700

    Doan so

--- baitap/doanso.py
+++ baitap/doanso.py
@@ -2,11 +2,11 @@ number = input("Enter any number: ")
 
 number_sqr = input("Enter square of {}: ".format(number))
 if int(number_sqr) > int(number)**2:
-        print ("too big")
+        print ("Too big")
 elif int(number_sqr) == int(number)**2:
 	print("Congratulation")
 else:
 	if int(number_sqr) < int(number)**2:
-		print("too low")
+		print("Too low")
 	else:
 		print("Try again!")
