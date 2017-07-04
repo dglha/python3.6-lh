39ec6a2736552605cc42478e53e69908c527d6ad
Duong Le Ha <duongleha212001@gmail.com>
2017-07-04 22:18:22 +0700

    Print Calendar

---
+++ scripts/calendar/cadar.py
@@ -0,0 +1,7 @@
+import calendar
+year = int(input())
+if 1>year or year >99999:
+    print("please choose another year")
+else:
+    for i in range (12):
+            print(calendar.month(year,i+1))
--- scripts/calendar/calendar.py
+++
@@ -1,8 +0,0 @@
-import calendar
-# choose a year between 1 to 9999
-    y= int(input())
-    if 1>y or y>99999:
-        print("please choose another year")
-    else:
-        for i in range (12):
-            print(calendar.month(y,i+1))
